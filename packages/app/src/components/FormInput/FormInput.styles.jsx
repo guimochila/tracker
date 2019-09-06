32351e2eea63c357f4b48fd4fad966c844623894
Marlon Santos <Marlonergondossantos@gmail.com>
2019-09-06 08:07:25 -0300

    Update FormInput.styles.jsx

--- packages/app/src/components/FormInput/FormInput.styles.jsx
+++ packages/app/src/components/FormInput/FormInput.styles.jsx
@@ -1,6 +1,5 @@
 import { StyleSheet } from 'react-native';
 
-const inputStyles = StyleSheet.create({
+export const inputStyles = StyleSheet.create({
   marginBottom: 30,
 });
-export default inputStyles;
