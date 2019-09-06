c5ed77b322f0e22f059c1e4487965fa024e1ffc0
git <marlonergondossantos@gmail.com>
2019-09-06 10:09:29 -0300

    eslint(no-unused-vars)

--- packages/app/src/screens/ResolveAuth/ResolveAuthScreen.js
+++ packages/app/src/screens/ResolveAuth/ResolveAuthScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react';
+import { useEffect, useContext } from 'react';
 import { Context as AuthContext } from '../../context/Auth';
 
 const ResolveAuthScreen = () => {
