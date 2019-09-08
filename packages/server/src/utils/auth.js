bdd5a7f8bc9c11b7f1a8376829a473c953e3493b
Guilherme <gui.mochila@gmail.com>
2019-09-08 15:55:31 +0200

    Old signin and signout methods were removed

--- packages/server/src/utils/auth.js
+++ packages/server/src/utils/auth.js
@@ -1,37 +1,6 @@
-import jwt, { sign, verify } from 'jsonwebtoken';
-import { isEmail, isEmpty, isLength } from 'validator';
+import { sign, verify } from 'jsonwebtoken';
 
 import User from '../types/user/user.model';
-
-export const signup = async (req, res) => {
-  const { email, password, name } = req.body;
-
-  if (!name || isEmpty(name)) {
-    return res.status(400).json({ error: 'You must provide a name' });
-  }
-
-  if (!email || !isEmail(email)) {
-    return res.status(400).json({ error: 'You must provide a valid email' });
-  }
-
-  if (!isLength(password, { min: 6, max: 30 })) {
-    return res
-      .status(400)
-      .json({ error: 'Password must have between 6 and 30 characters' });
-  }
-
-  try {
-    const user = await User.create({ name, email, password });
-    const token = jwt.sign({ userId: user.id }, process.env.SECRET_KEY);
-
-    return res.send({ token });
-  } catch (error) {
-    if (error.message.includes('duplicate key error')) {
-      return res.status(422).json({ error: 'Email is already in use' });
-    }
-    return res.status(500).json({ error: error.message });
-  }
-};
 
 export const generateToken = (user, expiresIn) => {
   return sign(
@@ -89,36 +58,5 @@ export const isAuthenticated = async (req, res, next) => {
     res.cookie('accessToken', newAccessToken);
 
     return next();
-  }
-};
-
-export const signin = async (req, res) => {
-  const { email, password } = req.body;
-
-  if (!email || !password || !isEmail(email)) {
-    return res
-      .status(422)
-      .json({ error: 'You must provide an email and password' });
-  }
-
-  const user = await User.findOne({ email });
-
-  if (!user) {
-    return res.status(401).json({ error: 'Invalid email or password' });
-  }
-
-  try {
-    const isPasswordValid = await user.comparePassword(password);
-
-    if (!isPasswordValid) throw Error;
-
-    const token = jwt.sign(
-      { userId: user.id, email: user.email, name: user.name },
-      process.env.SECRET_KEY,
-    );
-
-    return res.json({ token });
-  } catch (error) {
-    return res.status(401).json({ error: 'Invalid email or password' });
   }
 };
